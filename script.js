5f2a3e6c95f58d378a4b18e1eeaa1642f0415cd5
Kamil6747 <kamlbaron3523@gmail.com>
2025-09-24 14:09:56 +0200

    v4

--- script.js
+++ script.js
@@ -102,6 +102,90 @@ const weightInput = document.getElementById('weightInput');
     );`
     }
 
+    const line1 = document.getElementById("line1")
+    line1.onclick = function(){
+      line1.style.backgroundColor = "#7dd942";
+    }
+    const line2 = document.getElementById("line2")
+    line2.onclick = function(){
+      line2.style.backgroundColor = "#5dbf23";
+    }
+    const line3 = document.getElementById("line3")
+    line3.onclick = function(){
+      line3.style.backgroundColor = "#459818";
+    }
+    const line4 = document.getElementById("line4")
+    line4.onclick = function(){
+      line4.style.backgroundColor = "#367417";
+      
+    }
+    
+    const line5 = document.getElementById("line5")
+    line5.onclick = function(){
+      line5.style.backgroundColor = "#2e5c18";
+    }
+
+
+    const barChart = [
+      {
+        name:'Bar1' ,value: 130
+      },
+      {
+         name:'Bar2' ,value: 180,
+      },
+      {
+        name:'Bar3' ,value: 70,
+      },
+      {
+         name:'Bar4' ,value: 250,
+      },
+      {
+        name:'Bar5' ,value: 190,
+      }
+      
+    ];
+
+
+     const bar1 = document.getElementById("bar1");
+    bar1.style.height = barChart[0].value + "px";
+
+
+    const bar2 = document.getElementById("bar2");
+    bar2.style.height = barChart[1].value + "px";
+
+   const bar3 = document.getElementById("bar3");
+    bar3.style.height = barChart[2].value + "px";
+
+    const bar4 = document.getElementById("bar4");
+    bar4.style.height = barChart[3].value + "px";
+
+    const bar5 = document.getElementById("bar5");
+    bar5.style.height = barChart[4].value + "px";
+
+
+    const PieDonut = [
+     {
+        name:'pie1' ,value: 130
+      },
+      {
+         name:'pie2' ,value: 180,
+      },
+      {
+        name:'pie3' ,value: 70,
+      },
+      {
+         name:'pie4' ,value: 250,
+      }
+    
+    ];
+
+    const piedonut = document.getElementById("piedonut");
+
+    piedonut.style.backgroundImage = conic-gradient(
+    );
+    
+
+
     
 
     
